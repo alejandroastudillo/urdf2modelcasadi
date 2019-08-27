6c354c1b4b576ee387069d05bcebf746cc5cfad6
Alejandro ASTUDILLO VIGOYA <alejandro.astudillo@correounivalle.edu.co>
2019-08-27 13:02:29 +0200

    completed basic_example

--- urdf2model/examples/basic_example.cpp
+++ urdf2model/examples/basic_example.cpp
@@ -4,16 +4,39 @@
 int main()
 {
     // Example with Kinova Gen3 URDF.
-      std::string urdf_filename = "../urdf2model/models/kortex_description/urdf/JACO3_URDF_V11.urdf";
 
+    // ---------------------------------------------------------------------
+    // Create a model based on a URDF file
+    // ---------------------------------------------------------------------
+      std::string urdf_filename = "../urdf2model/models/kortex_description/urdf/JACO3_URDF_V11.urdf";
     // Instantiate a Serial_Robot object called robot_model
       mecali::Serial_Robot robot_model;
     // Create the model based on a URDF file
       robot_model.import_model(urdf_filename);
 
+    // ---------------------------------------------------------------------
+    // Look inside the robot_model object. What variables can you fetch?
+    // ---------------------------------------------------------------------
+    // Get some variables contained in the robot_model object
+      std::string name      = robot_model.name;
+      int         n_q       = robot_model.n_q;
+      int         n_joints  = robot_model.n_joints;
+      int         n_dof     = robot_model.n_dof;
+      int         n_frames  = robot_model.n_frames;
+      std::vector<std::string> joint_names = robot_model.joint_names;
+      std::vector<std::string> joint_types = robot_model.joint_types;
+      Eigen::VectorXd          gravity     = robot_model.gravity;
+      Eigen::VectorXd          joint_torque_limit    = robot_model.joint_torque_limit;
+      Eigen::VectorXd          joint_pos_ub          = robot_model.joint_pos_ub;
+      Eigen::VectorXd          joint_pos_lb          = robot_model.joint_pos_lb;
+      Eigen::VectorXd          joint_vel_limit       = robot_model.joint_vel_limit;
+      Eigen::VectorXd          neutral_configuration = robot_model.neutral_configuration;
     // Print some information related to the imported model (boundaries, frames, DoF, etc)
       robot_model.print_model_data();
 
+    // ---------------------------------------------------------------------
+    // Set functions for robot dynamics and kinematics
+    // ---------------------------------------------------------------------
     // Set function for forward dynamics
       casadi::Function fwd_dynamics = robot_model.forward_dynamics();
     // Set function for inverse dynamics
@@ -36,6 +59,21 @@ int main()
       casadi::Function fk_pos_oneframe_by_name   = robot_model.forward_kinematics("position", "EndEffector_Link");
       casadi::Function fk_pos_oneframe_by_index  = robot_model.forward_kinematics("position", 18);
 
+    // ---------------------------------------------------------------------
+    // Generate random configuration vectors
+    // ---------------------------------------------------------------------
+      // You can generate a random configuration vector (size n_q) that takes into account upper and lower boundaries of the joint angles.
+      Eigen::VectorXd random_conf_vector = robot_model.randomConfiguration();
+      // You can also use your own upper and lower boundaries of the joint angles using Eigen::VectorXd of size n_dof,
+      Eigen::VectorXd random_conf_vector_bounded = robot_model.randomConfiguration(-0.94159*Eigen::VectorXd::Ones(robot_model.n_dof), 0.94159*Eigen::VectorXd::Ones(robot_model.n_dof));
+      // or using std::vector<double> of size n_dof.
+      Eigen::VectorXd random_conf_vector_bounded2 = robot_model.randomConfiguration(std::vector<double>{-2, -2.2, -3.0, -2.4, -2.5, -2.6, -1}, std::vector<double>{2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 0.75});
+
+      // mecali::print_indent("Random configuration (bounded) = ", random_conf_vector_bounded, 38);
+
+    // ---------------------------------------------------------------------
+    // Evaluate a kinematics or dynamics function
+    // ---------------------------------------------------------------------
     // Test a function with numerical values
       /* Create a std::vector<double> of size robot_model.n_q (take care in case there are continuous joints in your model)
 
@@ -43,24 +81,28 @@ int main()
          Continuous joints are not represented just by q_i, but by [cos(q_i), sin(q_i)].
          The configuration vector is then set as: [cos(q1), sin(q1), q2, cos(q3), sin(q3), q4, cos(q5), sin(q5), q6, cos(q7), sin(q7)]
       */
-      std::vector<double> q_vec = {0.86602540378, 0.5, 0, 1, 0, -0.45, 1, 0, 0, 1, 0};
+      std::vector<double> q_vec = {0.86602540378, 0.5, 0, 1, 0, -0.45, 1, 0, 0.2, 1, 0};
       // Evaluate the function with a casadi::DMVector containing q_vec as input
       casadi::DM pos_res = fk_pos_oneframe_by_name(casadi::DMVector {q_vec})[0];
       std::cout << "Function result with q_vec input        : " << pos_res << std::endl;
 
-      // You can also use robot's neutral configuration or any random configuration as input
+      // You can also use robot's neutral configuration as input
       std::vector<double> q_vec_neutral((size_t)robot_model.n_q);
       Eigen::Map<mecali::ConfigVector>( q_vec_neutral.data(), robot_model.n_q, 1 ) = robot_model.neutral_configuration; // Populate q_vec_neutral with the robot's neutral configuration
 
+      casadi::DM pos_neutral = fk_pos_oneframe_by_name(casadi::DMVector {q_vec_neutral})[0];
+      std::cout << "Function result with q_vec_neutral input: " << pos_neutral << std::endl;
+
+      // or use a random configuration as input
       std::vector<double> q_vec_random((size_t)robot_model.n_q);
       Eigen::Map<mecali::ConfigVector>( q_vec_random.data(), robot_model.n_q, 1 ) = robot_model.randomConfiguration(); // Populate q_vec_neutral with a random configuration
 
-      casadi::DM pos_neutral = fk_pos_oneframe_by_name(casadi::DMVector {q_vec_neutral})[0];
       casadi::DM pos_random  = fk_pos_oneframe_by_name(casadi::DMVector {q_vec_random})[0];
-
-      std::cout << "Function result with q_vec_neutral input: " << pos_neutral << std::endl;
       std::cout << "Function result with q_vec_random input : " << pos_random  << std::endl;
 
+    // ---------------------------------------------------------------------
+    // Generate (or save) a function
+    // ---------------------------------------------------------------------
     // Code-generate or save a function
       // If not setting options, function fk_T_1 (or any function) will only be C-code-generated as "first_function.c" (or any other name you set)
       mecali::generate_code(fk_T_1, "first_function");

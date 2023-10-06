c4ef02b9adb2fcb955bdf8ab96f62edca04ea502
SHKim-HYU <tjsghd101@naver.com>
2023-10-06 16:21:40 +0900

    Indy model update

--- urdf2model/examples/turtlebot3_burger_example.cpp
+++ urdf2model/examples/turtlebot3_burger_example.cpp
@@ -9,11 +9,11 @@ int main()
   // ---------------------------------------------------------------------
   // Create a model based on a URDF file
   // ---------------------------------------------------------------------
-  std::string urdf_filename = ws_path+"/urdf2model/models/indy7/indy7.urdf";
+  std::string urdf_filename = ws_path+"/urdf2model/models/turtlebot3/turtlebot3_burger.urdf";
   // Instantiate a Serial_Robot object called robot_model
   mecali::Serial_Robot robot_model;
   // Define (optinal) gravity vector to be used
-  Eigen::Vector3d gravity_vector(0, 0, 0);
+  Eigen::Vector3d gravity_vector(0, 0, -9.81);
   // Eigen::Vector3d gravity_vector(0, 0, 0);
   // Create the model based on a URDF file
   robot_model.import_model(urdf_filename, gravity_vector);
@@ -39,9 +39,9 @@ int main()
   casadi::Function C = robot_model.coriolis_matrix();
   casadi::Function G = robot_model.generalized_gravity();
   // // Set function for forward kinematics
-  std::vector<std::string> required_Frames = {"joint0", "joint1", "joint2", "joint3", "joint4", "joint5", "indy7_tcp"};
+  std::vector<std::string> required_Frames = {"lin_x_joint", "lin_y_joint", "rot_z_joint", "base_link"};
 
-  std::string end_effector_name = "indy7_tcp";
+  std::string end_effector_name = "base_link";
 
   casadi::Function fkpos_ee = robot_model.forward_kinematics("position", end_effector_name);
   casadi::Function fkrot_ee = robot_model.forward_kinematics("rotation", end_effector_name);
@@ -50,7 +50,7 @@ int main()
 
   casadi::Function J_fd = robot_model.forward_dynamics_derivatives("jacobian");
   casadi::Function J_id = robot_model.inverse_dynamics_derivatives("jacobian");
-
+  
   casadi::Function J_s = robot_model.kinematic_jacobian("space", end_effector_name);
   casadi::Function J_b = robot_model.kinematic_jacobian("body", end_effector_name);
 
@@ -97,26 +97,25 @@ int main()
   // ---------------------------------------------------------------------
   // Code-generate or save a function
   // If you use options, you can set if you want to C-code-generate the function, or just save it as "second_function.casadi" (which can be loaded afterwards using casadi::Function::load("second_function.casadi"))
-  
   mecali::Dictionary codegen_options;
   codegen_options["c"] = true;
   codegen_options["save"] = true;
-  mecali::generate_code(fd, "indy7_fd", codegen_options);
-  mecali::generate_code(id, "indy7_id", codegen_options);
-  mecali::generate_code(M, "indy7_M", codegen_options);
-  mecali::generate_code(Minv, "indy7_Minv", codegen_options);
-  mecali::generate_code(C, "indy7_C", codegen_options);
-  mecali::generate_code(G, "indy7_G", codegen_options);
+  mecali::generate_code(fd, "turtlebot3_burger_fd", codegen_options);
+  mecali::generate_code(id, "turtlebot3_burger_id", codegen_options);
+  mecali::generate_code(M, "turtlebot3_burger_M", codegen_options);
+  mecali::generate_code(Minv, "turtlebot3_burger_Minv", codegen_options);
+  mecali::generate_code(C, "turtlebot3_burger_C", codegen_options);
+  mecali::generate_code(G, "turtlebot3_burger_G", codegen_options);
   //mecali::generate_code(fk_ee_pos, "mmo500_ppr_fk_ee_pos", codegen_options);
-   mecali::generate_code(fkrot_ee, "indy7_fkrot_ee", codegen_options);
-  mecali::generate_code(fk_ee, "indy7_fk_ee", codegen_options);
-  mecali::generate_code(fk, "indy7_fk", codegen_options);
-  mecali::generate_code(J_fd, "indy7_J_fd", codegen_options);
-  mecali::generate_code(J_id, "indy7_J_id", codegen_options);
-  mecali::generate_code(J_s, "indy7_J_s", codegen_options);
-  mecali::generate_code(J_b, "indy7_J_b", codegen_options);
+   mecali::generate_code(fkrot_ee, "turtlebot3_burger_fkrot_ee", codegen_options);
+  mecali::generate_code(fk_ee, "turtlebot3_burger_fk_ee", codegen_options);
+  mecali::generate_code(fk, "turtlebot3_burger_fk", codegen_options);
+  mecali::generate_code(J_fd, "turtlebot3_burger_J_fd", codegen_options);
+  mecali::generate_code(J_id, "turtlebot3_burger_J_id", codegen_options);
+  mecali::generate_code(J_s, "turtlebot3_burger_J_s", codegen_options);
+  mecali::generate_code(J_b, "turtlebot3_burger_J_b", codegen_options);
 
-  robot_model.generate_json("indy7.json");
+  robot_model.generate_json("turtlebot3_burger.json");
 
   // std::cout << fd << std::endl;
 }
